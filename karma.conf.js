07a69c602b2934b1411b47a95a6dc439f686effb
ThinaticSystem <git@thinaticsystem.com>
2022-03-30 05:32:04 +0900

    rename project name 2

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/honi'),
+      dir: require('path').join(__dirname, './coverage/thinaticsystem-com'),
       subdir: '.',
       reporters: [
         { type: 'html' },
