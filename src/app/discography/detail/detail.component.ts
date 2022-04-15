43ceda7dc7ac9f60fcca9e2fcf859cd2d00167c7
Cookie Ramen <ark@arkjp.net>
2022-04-16 05:09:10 +0900

    クリーンアップ

--- src/app/discography/detail/detail.component.ts
+++ src/app/discography/detail/detail.component.ts
@@ -9,47 +9,47 @@ import {ClipboardService} from "ngx-clipboard";
 import {NotificationService} from "../../service/notification.service";
 
 @Component({
-    selector: 'app-detail',
-    templateUrl: './detail.component.html',
-    styleUrls: ['./detail.component.scss']
+  selector: 'app-detail',
+  templateUrl: './detail.component.html',
+  styleUrls: ['./detail.component.scss']
 })
 export class DetailComponent implements OnInit, OnDestroy {
-    discography!: Discography;
-    environment = environment;
-    url = location.href;
-    id?: string | null;
+  discography!: Discography;
+  environment = environment;
+  url = location.href;
+  id?: string | null;
 
-    constructor(
-        private titleService: Title,
-        private route: ActivatedRoute,
-        private httpClient: HttpClient,
-        private sanitizer: DomSanitizer,
-        private router: Router,
-        private clipboard: ClipboardService,
-        public loadingService: LoadingService,
-        public Notification: NotificationService,
-    ) {
-    }
+  constructor(
+    private titleService: Title,
+    private route: ActivatedRoute,
+    private httpClient: HttpClient,
+    private sanitizer: DomSanitizer,
+    private router: Router,
+    private clipboard: ClipboardService,
+    public loadingService: LoadingService,
+    public Notification: NotificationService,
+  ) {
+  }
 
-    ngOnInit(): void {
-        // 仮タイトル設定
-        this.titleService.setTitle('Discography | しなちくシステム');
-        // URLからIDを取得
-        this.id = this.route.snapshot.paramMap.get('id');
+  ngOnInit(): void {
+    // 仮タイトル設定
+    this.titleService.setTitle('Discography | しなちくシステム');
+    // URLからIDを取得
+    this.id = this.route.snapshot.paramMap.get('id');
 
-        // IDからDiscographyを取得
-        this.httpClient.get<Discography>(`${environment.cmsUrl}/discographies/${this.id}`)
-            .subscribe((data) => {
-                this.discography = data;
-                // タイトル設定
-                this.titleService.setTitle(`${this.discography.title} | しなちくシステム`);
-            }, (error) => {
-                // 詳細情報が存在しない場合はDiscography一覧にリダイレクト
-                this.router.navigate(['/discography']);
-            });
-    }
+    // IDからDiscographyを取得
+    this.httpClient.get<Discography>(`${environment.cmsUrl}/discographies/${this.id}`)
+      .subscribe((data) => {
+        this.discography = data;
+        // タイトル設定
+        this.titleService.setTitle(`${this.discography.title} | しなちくシステム`);
+      }, (error) => {
+        // 詳細情報が存在しない場合はDiscography一覧にリダイレクト
+        this.router.navigate(['/discography']);
+      });
+  }
 
-    ngOnDestroy(): void {
-        this.loadingService.loading = true;
-    }
+  ngOnDestroy(): void {
+    this.loadingService.loading = true;
+  }
 }
