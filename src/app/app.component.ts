e08ccb0b5b0305533be1574ba62eab7f58855857
ThinaticSystem <git@thinaticsystem.com>
2022-04-18 21:30:39 +0900

    フッターリンク追加&調整

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,22 +39,25 @@ export class AppComponent {
   footerLinks = [
     {
       name: 'ActivityPub',
-      url: 'https://honi.club/@ThinaticSystem/'
+      url: 'https://honi.club/@ThinaticSystem'
     }, {
       name: 'SoundCloud',
-      url: 'https://soundcloud.com/ThinaticSystem/'
+      url: 'https://soundcloud.com/thinaticsystem'
     }, {
       name: 'Discord',
       url: 'https://discord.com/users/260449322183819264'
     }, {
       name: 'Twitter',
-      url: 'https://twitter.com/ThinaticSystem/'
+      url: 'https://twitter.com/ThinaticSystem'
     }, {
-      name: 'Feed エサ',
-      url: 'http://amzn.asia/d8Q1ib7'
+      name: 'Instagram',
+      url: 'https://www.instagram.com/thinaticsystem/'
     }, {
       name: 'Patreon',
       url: 'https://www.patreon.com/ThinaticSystem'
+    }, {
+      name: 'エサをやる',
+      url: 'http://amzn.asia/d8Q1ib7'
     }
   ];
 
