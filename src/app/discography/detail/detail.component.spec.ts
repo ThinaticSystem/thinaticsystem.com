5f763051651fdb5bc2bfe3cca5754bbdc3365dc4
しなちくシステム <git@thinaticsystem.com>
2024-05-03 09:35:38 +0000

    refactor: standalone @ Discography

--- src/app/discography/detail/detail.component.spec.ts
+++ src/app/discography/detail/detail.component.spec.ts
@@ -1,6 +1,5 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
-
-import {DetailComponent} from './detail.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import DetailComponent from './detail.component';
 
 describe('DetailComponent', () => {
   let component: DetailComponent;
